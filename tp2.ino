8aba27c19f45e63e0663405bd20f419cbc917531
Heng <e1869960@cmaisonneuve.qc.ca>
2025-02-11 09:43:27 -0500

    0-255

---
+++ tp2.ino
@@ -0,0 +1,28 @@
+//Kevin Heng
+//TP2 - Antenne motorisée
+
+
+#include <Servo.h>
+#include <Wire.h>
+#define CMPS12_ADDRESS 0x60
+Servo myServo;
+int positionServo;
+
+void setup() {
+  Serial.begin(9600);  // Start serial communication
+  Wire.begin();  // Start I2C communication
+}
+
+void loop() {
+  // Request heading data from CMPS12
+  Wire.beginTransmission(CMPS12_ADDRESS);
+  Wire.write(0x01);  // Register for 0-255 
+  Wire.endTransmission();
+  
+  Wire.requestFrom(CMPS12_ADDRESS,1);
+  if (Wire.available()){
+    int value = Wire.read();
+    Serial.println(value);
+  }
+  delay(100);  // Update every 100ms
+}
