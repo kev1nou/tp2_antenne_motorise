8d6dc36ea1084ef5896825f7dbf70003875725ae
kev1nou <123593938+kev1nou@users.noreply.github.com>
2025-02-21 14:58:28 -0500

    0-255 proportionnel fonctionnel

--- tp2.ino
+++ tp2.ino
@@ -5,7 +5,8 @@
 #include <Servo.h>
 #include <Wire.h>
 #define CMPS12_ADDRESS 0x60
-#define kp 5.083
+#define kp -5
+#define ki -10
 #define umin 900
 #define umax 2100
 
@@ -13,13 +14,13 @@ Servo myServo;
 int positionServo;
 
 int previousMillis = 0;
-int temps = 10; //Pour 10ms timer
-int angleSortie = 120;
+int temps = 10;  //Pour 10ms timer
+int angle_0_255 = 120;
 
 
 void setup() {
   Serial.begin(9600);  // Start serial communication
-  Wire.begin();  // Start I2C communication
+  Wire.begin();        // Start I2C communication
   myServo.attach(3);
 }
 
@@ -28,34 +29,37 @@ void loop() {
   Wire.beginTransmission(CMPS12_ADDRESS);
   Wire.write(0x01);  // Register 0-255
   Wire.endTransmission();
-  
+
 
   Wire.requestFrom(CMPS12_ADDRESS, 1);
-  if (Wire.available()) {
-    int heading = Wire.read();
 
-    // Map the heading (0-255)
-    int angle = map(heading, 0, 255, 0, 255);
+  int currentMillis = millis();
+  if (currentMillis - previousMillis >= temps) {  //Boucle de 10ms
+    previousMillis = currentMillis;
+    if (Wire.available()) {
+      int heading = Wire.read();
 
-    // Print the mapped value (0-255) to the serial monitor
-    Serial.print("Angle : ");
-    Serial.println(angle);
-    int currentMillis = millis();
-    if (currentMillis - previousMillis >= temps) { //Boucle de 10ms
-      previousMillis = currentMillis;
-    int erreur = angleSortie - angle;
-    erreur = erreur % 360;
-      if (erreur > 180){
-        erreur = erreur - 360;
+      // Map the heading (0-255)
+      int angle_mesure = map(heading, 0, 255, 0, 255);
+
+      int erreur = angle_0_255 - angle_mesure;
+
+      Serial.print("Angle : ");
+      Serial.println(angle_mesure);
+
+      erreur = erreur % 255;
+      if (erreur > 127) {
+        erreur = erreur - 255;
       }
-    int u = float(kp*erreur) + 1490;
-    myServo.writeMicroseconds(u);
-    u = u < umax ? umax : u;
-    u = u > umin ? umin : u;
-    
-    Serial.print("Erreur : ");
-    Serial.println(erreur);
-    }   
+      float u = float(kp * erreur) + 1490;
+      myServo.writeMicroseconds(u);
+      u = u < umax ? umax : u;
+      u = u > umin ? umin : u;
+
+      Serial.print("Erreur : ");
+      Serial.println(erreur);
+    }
   }
+
   delay(100);  // Update every 100ms
 }
